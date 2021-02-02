6b109f87aee94d6ee8a7b4f3a19bb9223f459cf6
Alattyani-Mark <70943568+Alattyani-Mark@users.noreply.github.com>
2021-02-02 10:32:50 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace labirinho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("M E G A K A R O K H A L N I");
+            Console.ReadKey();
+        }
+    }
+}
